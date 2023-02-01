578778ed60b06ca641447a3e44cff93edebed6ff
Ayoub Boudani <ayoubimajjati@gmail.com>
2023-02-01 20:17:05 +0100

    configuration de base de donnees

--- concour/settings.py
+++ concour/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'concour.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe',
+        'USER': 'system',
+        'PASSWORD': 'test',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
